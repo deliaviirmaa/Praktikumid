c91359d955177aa6a2157cce39bc84cdb502f12f
Delia Viirmaa <dviirmaa@klass2-13-l.intra.itcollege.ee>
2015-10-23 13:40:59 +0300

    Kuulujutu generaatori algus

---
+++ src/praktikum7/KuulujutuGeneraator.java
@@ -0,0 +1,14 @@
+package praktikum7;
+
+public class KuulujutuGeneraator {
+	String[] naisteNimed = {"Mari","Pille","Laine","Kartu"};
+	String[] meesteNimed = {"Kalle", "Kaspar", "Jyri","Aivar"};
+	String[] tegevused = {"armastab", "vihkab", "petab", "kallistab"};
+	
+	// public static String suvalineElement(String[] massiiv)
+	// git status (läbi lugeda), git add Praktikum7, git status, git commit, i - saan kirjutada kommentaari, väljumiseks esc
+	// :X - salvestas ja läks välja, git push. // git pull (kodus saan selle võtta) git clone, kui pole seda kasutanud 
+	// ja siis git pull /// pwd - näitab, kus kataloogis ma olen, siis cd
+	String lause = naiseNimed[random *length]
+
+}
