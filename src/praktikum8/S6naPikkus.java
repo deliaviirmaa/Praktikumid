8bc60a12b7cf235d92415c6f49a72744ba5add18
Delia Viirmaa <dviirmaa@klass2-23-l.intra.itcollege.ee>
2015-10-30 10:58:29 +0200

    Kaheksanda praktikumi esimene ülesanne on lahendatud

--- src/praktikum8/S6naPikkus.java
+++ src/praktikum8/S6naPikkus.java
@@ -1,10 +1,24 @@
 package praktikum8;
 
-import com.sun.java_cup.internal.runtime.Scanner;
+import java.util.Scanner;
 
 public class S6naPikkus {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		String[] s6nad = new String[10];
+		int indeks = 0;
+
+		while (indeks < s6nad.length) {
+			System.out.println("Palun sisestage sõna.");
+			String sisestatudS6na = scanner.nextLine();
+			int s6naPikkus = sisestatudS6na.length();
+			s6nad[indeks] = s6naPikkus +" "+ sisestatudS6na;
+			indeks++;
+		}
+	for(int i=0;i<s6nad.length;i++){
+		System.out.println(s6nad[i]);
+	}
+
 	}
 
 }
