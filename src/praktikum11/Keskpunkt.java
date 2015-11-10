f53172d54c92632d0293b15f0c55365072c44008
Delia Viirmaa <dviirmaa@klass2-23-l.intra.itcollege.ee>
2015-11-10 12:24:34 +0200

    Lisasin 11 praktikumi esimese ülesande

---
+++ src/praktikum11/Keskpunkt.java
@@ -0,0 +1,36 @@
+package praktikum11;
+
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Keskpunkt extends Applet {
+	/*
+	 * Ringjoone vo~rrand parameetrilisel kujul x = r * cos(t) y = r * sin(t) t
+	 * = -PI..PI
+	 */
+	public void paint(Graphics g) {
+		int x0 = getWidth()/2; // Keskpunkt
+		int y0 = getHeight()/2;
+		int r = 100; // Raadius
+		int x, y;
+		double t;  //nurk
+
+		// Kysime kui suur aken on?
+		int w = getWidth();
+		int h = getHeight();
+
+		// Ta"idame tausta
+		g.setColor(Color.red);
+		g.fillRect(0, 0, w, h);
+
+		// Joonistame
+		g.setColor(Color.black);
+
+		for (t = -Math.PI; t < Math.PI; t = t + Math.PI / 16) {
+			x = (int) (r * Math.cos(t) + x0);
+			y = (int) (r * Math.sin(t) + y0);
+			g.drawLine(x0, y0, x, y);
+		}
+	}
+}
